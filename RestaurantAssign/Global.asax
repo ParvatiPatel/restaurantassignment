25a94f60833a9534f9c7813290cb08c0ddf0ec1d
ParvatiPatel <paro.patel@rocketmail.com>
2016-07-21 09:37:30 -0400

    Add project files.

---
+++ RestaurantAssign/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestaurantAssign.MvcApplication" Language="C#" %>
---
+++ RestaurantAssign/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantAssign.Startup))]
+namespace RestaurantAssign
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
